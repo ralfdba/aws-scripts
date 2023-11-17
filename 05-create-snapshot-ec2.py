75e5ff56ad19bcf8fb9a1ced9f12f6baca8bf2aa
RalfXIV <2693917+ralfdba@users.noreply.github.com>
2023-11-17 13:22:19 -0300

    Update 05-create-snapshot-ec2.py

--- 05-create-snapshot-ec2.py
+++ 05-create-snapshot-ec2.py
@@ -1,17 +1,21 @@
 import boto3
-ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
-    resultado = ec2.describe_volumes( Filters=[{'Name': 'tag:status', 'Values': ['in-use']}])
-    for volume in resultado['Volumes']:
-        print( volume['VolumeId'], volume['AvailabilityZone'] )
-        resultado2 = ec2.create_snapshot(VolumeId=volume['VolumeId'],Description='Creado por lambda')
-        ec2resource = boto3.resource('ec2')
-        snapshot = ec2resource.Snapshot(resultado2['SnapshotId'])
-        if 'Tags' in volume:
-            for tags in volume['Tags']:
-                if tags["Key"] == 'Name':
-                    nombredisco = tags["Value"]
-        else:
-            nombredisco = 'SN'
-        snapshot.create_tags(Tags=[{'Key': 'Name','Value': nombredisco}])
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_volumes( Filters=[ { 'Name': 'tag:Name','Values': ['srv01'] } ] )
+    for volume in response['Volumes']:
+        ec2.create_snapshot( 
+            VolumeId=volume['VolumeId'], 
+            Description="Desde lambda",
+            TagSpecifications=[
+                {
+                    'ResourceType':'snapshot',
+                    'Tags':[
+                        {
+                            'Key': 'Name',
+                            'Value': 'desde-lambda-sp'
+                        }
+                    ]
+                }
+            ]
+        )
